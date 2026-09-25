d0960dcb4d65d8b88ffb259e558e072c8db3674f
copium.suffocation <copiumsuffocation@proton.me>
2026-09-25 15:42:30 +0700

    add game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        var games = new String[] {"Exit", "Greet"};
+        for (int i = 0; i < games.length; i++) {
+            System.out.printf("%d - %s\n", i, games[i]);
+        }
+
+        var scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        switch (input) {
+            case 0:
+                System.exit(0);
+                break;
+            case 1:
+                Cli.greeting();
+                break;
+        }
+        scanner.close();
     }
 }
